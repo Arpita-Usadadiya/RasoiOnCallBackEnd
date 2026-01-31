89c6d66d63b58ee4d0f7266347546648f5bf67e2
Arpita-Usadadiya <arpita1usadadiya4@gmail.com>
2026-01-31 22:29:51 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+//Load environment variables
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1); // stop app if DB fails
+  }
+};
+
+module.exports = connectDB;
